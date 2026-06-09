bb51631c622967192b4c66fb4a2e44257e5168d4
SanYuAung-KBZ <sanyu.aung@kbzbank.com>
2026-06-09 09:30:44 +0630

    feat: update page title for consistency in Modern Portfolio

--- src/app/modern/page.js
+++ src/app/modern/page.js
@@ -1,7 +1,7 @@
 import ModernPortfolioHome from "@/components/ModernPortfolioHome";
 
 export const metadata = {
-  title: "San Yu Aung || Modern Portfolio Website",
+  title: "Modern Portfolio Website",
 };
 
 export default function ModernPortfolio() {
